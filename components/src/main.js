4d0f8309c010326f99a779e444585213b741c52f
VarushSiva <varushsivalingam@gmail.com>
2025-09-23 17:28:21 -0400

    Add GameController Logic

--- components/src/main.js
+++ components/src/main.js
@@ -16,6 +16,17 @@ function gameBoard() {
     // Create a method to get entire board --> UI needs this to render board
     const getBoard = () => board;
 
+    // Display token when clicked on a valid cell
+    const displayToken = (row, col, player) => {
+        const availableCells = board.filter((row) => row[col].getValue() === "")
+
+        // if No cells are empty --> Return / Check for win condition
+        if (!availableCells.length) return;
+
+        // Else add token to the available cell
+        board[row][col].addToken(player);
+    }
+
     // Create a method to print the board to console
     // Helpful to seeing the board after each turn
     const printBoard = () => {
@@ -24,7 +35,7 @@ function gameBoard() {
     }
 
     // Return methods
-    return { getBoard, printBoard }
+    return { getBoard, displayToken, printBoard }
 
 }
 
@@ -36,7 +47,7 @@ function Cell() {
     let value = "";
 
     // Accept a players move to change the value of the cell
-    const validMove = (player) => {
+    const addToken = (player) => {
         value = player;
     } 
 
@@ -44,7 +55,7 @@ function Cell() {
     const getValue = () => value;
 
     // Return methods
-    return { validMove, getValue }
+    return { addToken, getValue }
 
 }
 
@@ -52,29 +63,58 @@ function Cell() {
 // Including Win Conditions
 function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
     // Set up gameboard
-    
+    const board = gameBoard();
 
     // Set up players
-
+    const player = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ]
 
     // Set active player
-
+    let activePlayer = player[0]
 
     // Set up turn control --> if Player 1 --> Next = Player 2
-
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player[0] ? player[1] : player[0]
+    }
 
     // Method to get current active player
+    const getActivePlayer = () => activePlayer;
 
-
-    // Print new round + Print/Log Players turn using method getActivePlayer
-
+    // Print new round 
+    const printNewRound = () => {
+        // Print board using method + Print/Log Players turn using method getActivePlayer 
+        board.getBoard();
+        console.log(`${getActivePlayer().name}'s Turn.`)
+    }
 
     // Play Round Logic
+    const playRound = (row, col) => {
+        // Add token for the current player
+        console.log(`Adding ${getActivePlayer().name}'s Token into row ${row}, column ${col}`);
+        // Use displayToken method from gameboard using parameters --> row + column + Player Token Identification
+        board.displayToken(row, col, getActivePlayer().token);
 
+        /* Check for Winner + Win Message Logic */
+
+
+        switchPlayerTurn();
+        printNewRound();
+    };
 
     // Initial play game message --> printNewRound();
+    printNewRound();
 
 
     // Return Methods:
     // Console playRound | UI Version = getActivePlayer + getBoard
+    return { playRound, getActivePlayer, getBoard: board.getBoard() }
 }
+
