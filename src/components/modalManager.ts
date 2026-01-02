92b73d25cbb910b638fda35e8000dd0e75dc2985
VarushSiva <varushsivalingam@gmail.com>
2026-01-02 02:54:56 -0500

    Create component file - modalManager

---
+++ src/components/modalManager.ts
@@ -0,0 +1,71 @@
+// ModalManager
+import { setModalState } from "./utils";
+
+export class ModalManager {
+  private activeFocus: Array<() => void> = [];
+
+  constructor(private onPause?: () => void, private onResume?: () => void) {}
+
+  open(
+    overlayElement: HTMLElement,
+    modalElement: HTMLElement,
+    focusElement: HTMLElement
+  ): void {
+    setModalState(overlayElement, true);
+
+    if (this.onPause) this.onPause();
+
+    const cleanup = this.setupFocus(modalElement);
+    if (cleanup) this.activeFocus.push(cleanup);
+
+    focusElement.focus();
+  }
+
+  close(overlayElement: HTMLElement, returnFocusElement?: HTMLElement): void {
+    setModalState(overlayElement, false);
+
+    this.clearFocus();
+
+    if (this.onResume) this.onResume();
+
+    if (returnFocusElement) {
+      setTimeout(() => returnFocusElement.focus(), 50);
+    }
+  }
+
+  // Focus Restrictions when modal open
+  private setupFocus(container: HTMLElement): (() => void) | void {
+    const focusableElements = container.querySelectorAll<HTMLElement>(
+      'button:not([disabled]), input:not([disabled]), select:not([disabled]), [tabindex]:not([tabindex="-1"])'
+    );
+
+    if (focusableElements.length === 0) return;
+
+    const firstElement = focusableElements[0];
+    const lastElement = focusableElements[focusableElements.length - 1];
+
+    const handleTabKey = (e: KeyboardEvent) => {
+      if (e.key !== "Tab") return;
+
+      if (e.shiftKey) {
+        if (document.activeElement === firstElement) {
+          e.preventDefault();
+          lastElement.focus();
+        }
+      } else {
+        if (document.activeElement === lastElement) {
+          e.preventDefault();
+          firstElement.focus();
+        }
+      }
+    };
+
+    container.addEventListener("keydown", handleTabKey);
+    return () => container.removeEventListener("keydown", handleTabKey);
+  }
+
+  private clearFocus(): void {
+    this.activeFocus.forEach((cleanup) => cleanup());
+    this.activeFocus = [];
+  }
+}
