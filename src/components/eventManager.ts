157a871b5b5ded64a65672645e59a6d9a6f994db
VarushSiva <varushsivalingam@gmail.com>
2026-01-02 09:25:20 -0500

    Create component file - eventManager

---
+++ src/components/eventManager.ts
@@ -0,0 +1,34 @@
+// EventManager
+type EventCallback = (event: Event) => void;
+
+interface RegisteredEvent {
+    element: Element;
+    event: string;
+    callback: EventCallback;
+}
+
+export class EventManager {
+    private events: RegisteredEvent[] = [];
+
+    register(element: Element, event: string, callback: EventCallback): void {
+        element.addEventListener(event, callback);
+        this.events.push({ element, event, callback });
+    }
+
+    unregister(element: Element, event: string, callback?: EventCallback): void {
+        this.events = this.events.filter((registered) => {
+            if (registered.element === element && registered.event === event && (!callback || registered.callback === callback)) {
+                element.removeEventListener(event, registered.callback);
+                return false;
+            }
+            return true;
+        });
+    }
+
+    cleanup(): void {
+        this.events.forEach(({ element, event, callback }) => {
+            element.removeEventListener(event, callback);
+        });
+        this.events = [];
+    }
+}
